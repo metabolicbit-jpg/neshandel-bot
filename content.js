64895fd2a007062fd1fe0818e1eb1ec0f7e57d35
metabolicbit-jpg <metabolicbit@gmail.com>
2026-08-22 13:18:53 +0330

    Delete content.js

--- content.js
+++
@@ -1,8 +0,0 @@
-// content.js — تجمیع‌کننده (همیشه کوچک می‌ماند)
-import { CONTENT as OLD } from "./content_old.js";
-// import { B1 } from "./content/p001-019.js";
-
-export const CONTENT = [
-  ...OLD,
-  // ...B1,
-];
