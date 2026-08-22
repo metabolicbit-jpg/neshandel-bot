71f53ebf9f859e297d290fb343aeaac0c41378fb
metabolicbit-jpg <metabolicbit@gmail.com>
2026-08-22 13:34:10 +0330

    Update index.js

--- content/index.js
+++ content/index.js
@@ -1,8 +1,6 @@
 // content/index.js — تجمیع‌کنندهٔ محتوا (همیشه کوچک می‌ماند)
-import { CONTENT as OLD } from "../content_old.js";
-// import { B1 } from "./p001-019.js";
+import { B1 } from "./p001-019.js";
 
 export const CONTENT = [
-  ...OLD,
-  // ...B1,
+  ...B1,
 ];
