71724457316b56ab15053248400ea6305ba0d986
metabolicbit-jpg <metabolicbit@gmail.com>
2026-08-27 08:48:17 +0330

    Update index.js

--- content/index.js
+++ content/index.js
@@ -1,7 +1,3 @@
+// content/index.js
 import { PILOT } from "./pilot.js";
-// import { B1 } from "./p001-019.js";
-// import { B2 } from "./p021-039.js";
-
-export const CONTENT = [
-  ...PILOT,
-];
+export const CONTENT = PILOT;
